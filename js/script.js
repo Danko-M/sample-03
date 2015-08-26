522c512e2cb4301a3af6be50fbf3a3651bb7d5fe
Danko-M <dankomilutinovic@gmail.com>
2015-08-26 22:47:02 +0200

    added some images

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 $(function() {
+	var trueWidth = window.innerWidth;
   function resizeBg() {
-    if ($(window).width() >= 1200) {
+    if (trueWidth >= 1200) {
       $(".landing").backstretch("img/zoom-29719860-3.jpg", {
-        centeredX: true
-      }).show();
+        centeredX: true,
+        fade : "normal"
+      });
     }
   }
 
   $(window).on("resize", resizeBg);
   resizeBg();
+  $(".c01").backstretch("img/city01b.jpg");
+  $(".c02").backstretch("img/city03b.jpg");
 });
