00f57910e04e515e2436c539741d60b9b3e2ffb2
Danko-M <dankomilutinovic@gmail.com>
2015-08-29 23:14:53 +0200

    added some animation

--- js/script.js
+++ js/script.js
@@ -3,4 +3,11 @@ $(function() {
   $(".landing").backstretch("img/hero.jpg");
   $(".c01").backstretch("img/city01.jpg");
   $(".c02").backstretch("img/city03.jpg");
+
+  		var config = { 
+  			reset:  true,
+  			mobile: false 
+  		};
+
+  		window.sr = new scrollReveal(config);
 });
