a71b1085f7025f1afe1a9b05498b6e9380e05963
Danko-M <dankomilutinovic@gmail.com>
2015-08-31 16:24:42 +0200

    added 'contact us' section

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 $(function() {
 	// full size width backgrounds
-  $(".landing").backstretch("img/hero.jpg");
+  $(".hero").backstretch("img/hero.jpg");
   $(".c01").backstretch("img/city01.jpg");
   $(".c02").backstretch("img/city03.jpg");
 
-  		var config = { 
-  			reset:  true
-  		};
+  var config = {};
+  
  	if(window.innerWidth >= 992) {
   	window.sr = new scrollReveal(config);
 	}
